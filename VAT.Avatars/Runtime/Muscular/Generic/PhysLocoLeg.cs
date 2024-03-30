0e0167930e770678d85c2365f4f196f1844d6e1f
Logan Shockley <loganv182@gmail.com>
2024-03-29 23:36:04 -0500

    Improved fender force

--- VAT.Avatars/Runtime/Muscular/Generic/PhysLocoLeg.cs
+++ VAT.Avatars/Runtime/Muscular/Generic/PhysLocoLeg.cs
@@ -177,7 +177,7 @@ namespace VAT.Avatars.Muscular
             {
                 positionSpring = 900000f,
                 positionDamper = 200000f,
-                maximumForce = 6000f * legScalar,
+                maximumForce = 6000f * legScalar * Mathf.Sqrt(legScalar),
             };
         }
 
